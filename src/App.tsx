46628510b54331d177fc198c1c4635273eb3570f
Alex Barclay <adbarclay92@gmail.com>
2024-02-10 19:37:39 -0700

    Pausing

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,81 @@ import { useState } from "react";
 import "./App.css";
 import AnalogClock from "./components/Clock/AnalogClock";
 import WorkerStatus from "./components/WorkerStatus";
+import {
+  ChakraProvider,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+
+const enum LoginState {
+  LoggedOut,
+  LoggedIn,
+  Error,
+  Loading,
+}
 
 function App() {
   const [jwt, setJwt] = useState("");
   const [employeeId, setEmployeeId] = useState("");
+  const [loginState, setLoginState] = useState(LoginState.LoggedOut);
 
-  const onEmployeeIdTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmployeeId(e.target.value);
   };
 
+  const onEmployeeIdSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setJwt("jwt-token-value");
+    // FIXME: make network request; onError, set error state
+  };
+
   return (
-    <>
-      <AnalogClock />
-      <h1>CardAgain</h1>
-      <div>
-        <>
-          {jwt.length > 0 ? (
-            <WorkerStatus />
-          ) : (
-            <>{/** Here goes the login component */}</>
-          )}
-        </>
-      </div>
-    </>
+    <ChakraProvider>
+      <>
+        <AnalogClock />
+        <h1>CardAgain</h1>
+        <div>
+          <>
+            {jwt.length > 0 ? (
+              <WorkerStatus />
+            ) : (
+              <>
+                <FormControl>
+                  <FormControl isInvalid={loginState === LoginState.Error}>
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                      type="email"
+                      value={input}
+                      onChange={handleInputChange}
+                    />
+                    {!isError ? (
+                      <FormHelperText>
+                        Enter the email you'd like to receive the newsletter on.
+                      </FormHelperText>
+                    ) : (
+                      <FormErrorMessage>
+                        Invalid employee ID.{" "}
+                        {/** FIXME: should create button to navigate to  */}
+                      </FormErrorMessage>
+                    )}
+                  </FormControl>
+                  <Button
+                    colorScheme="blue"
+                    onClick={() => setJwt("jwt-token-value")}
+                  >
+                    Login
+                  </Button>
+                </FormControl>
+              </>
+            )}
+          </>
+        </div>
+      </>
+    </ChakraProvider>
   );
 }
 
